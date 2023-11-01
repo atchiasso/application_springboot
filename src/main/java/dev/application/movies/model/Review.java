eace98e54c0459718b55f0725f5fbef7a3779a8e
atchiasso <a.tchiasso@gmail.com>
2023-11-01 17:23:22 +0100

    Implementation entity class Movie and Review

--- src/main/java/dev/application/movies/model/Review.java
+++ src/main/java/dev/application/movies/model/Review.java
@@ -1,2 +1,18 @@
-package dev.application.movies.model;public class Review {
+package dev.application.movies.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+
+    private ObjectId id;
+
+    private String body;
 }
